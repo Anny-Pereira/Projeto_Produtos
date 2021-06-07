5d12f43fdeda099e2c17b7b9d410316588c3a9ba
Anny Pereira <isabelle.pereira04@gmail.com>
2021-06-07 15:37:39 -0300

    Classe e Interface - Marca

--- Classes/Marca.cs
+++ Classes/Marca.cs
@@ -12,19 +12,80 @@ namespace Projeto_Produtos_0706.Classes
 
         private DateTime DataCadastro { get; set; }
 
+        public int i;
+
+        private bool RepDeletar = false;
+
+        List<Marca> marcas = new List<Marca>();
+
         public string Cadastrar(Marca Marca)
         {
-            throw new NotImplementedException();
+            Marca marca = new Marca();
+
+            i++;
+            marca.Codigo = i;
+
+            Console.Write($"Data de cadastro: {DataCadastro}");
+
+            Console.Write($"Código da marca: {marca.Codigo}");
+
+
+            Console.Write("\nInsira o nome da marca: ");
+            marca.NomeMarca = Console.ReadLine();
+
+
+            marcas.Add(marca);
+
+            return "Marca cadastrada!";
         }
 
         public string Deletar(Marca Marca)
         {
-            throw new NotImplementedException();
+            while (!RepDeletar)
+            {
+                Console.WriteLine("\nDeseja deletar algum item (s-sim / n-não)?");
+                string RespDeletar = Console.ReadLine();
+
+                if (RespDeletar == "s")
+                {
+                    RepDeletar = true;
+                    Console.WriteLine("\nQual marca deseja remover?");
+                    string MarcaDeletada = Console.ReadLine().ToLower();
+
+                    Marca MarcaEncontrada = marcas.Find(x => x.NomeMarca == MarcaDeletada);
+
+                    marcas.Remove(MarcaEncontrada);
+                }
+
+                else if (RespDeletar == "n") 
+                {
+                    RepDeletar = true;
+                }
+
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nNenhuma opção identificada. Tente novamente!\n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    RepDeletar = false;
+                }
+            }
+
+            return "Marca deletada!";
         }
 
         public List<Marca> Listar()
         {
-            throw new NotImplementedException();
+            foreach (var M in marcas)
+            {
+                Console.WriteLine($@"
+    {M.DataCadastro}
+    {M.Codigo}
+    {M.NomeMarca}
+                ");
+            }
+
+            return marcas;
         }
     }
 }
